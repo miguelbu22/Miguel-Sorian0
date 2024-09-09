691be92877199c5e8dc139953bc6706c1538ea5c
miguelbu22 <msoriano18@student.cccd.edu>
2024-09-08 22:25:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# variable-starter-vxgrgu
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/miguelbu22/variable-starter-vxgrgu)
---
+++ variable.js
@@ -0,0 +1,17 @@
+// TODO: Create a variable named `firstName` and assign your first name to it in the line below.
+let firstName = 'Miguel';
+
+// TODO: Create a variable named `lastName` and assign your last name to it in the line below.
+let lastName = 'Soriano';
+
+// TODO: Create a variable named `sayHello` and build the following string based on the variables
+// above: "Hello world! My name is <firstName> <lastName>. Nice to meet you!".
+let sayHello = 'Hello!';
+
+// DO NOT CHANGE ANYTHING BELOW THIS LINE.
+
+module.exports = {
+  firstName,
+  lastName,
+  sayHello,
+};
